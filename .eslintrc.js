7ea9f7744f27726ebb6279b6883d69251d5c9961
Erozak <pinex666@gmail.com>
2021-11-28 18:24:34 +0800

    chore(common): update project eslint with typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,18 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
+  rules: {},
   env: {
     node: true,
   },
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['./ts-config.js'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+    },
+  ],
 };
